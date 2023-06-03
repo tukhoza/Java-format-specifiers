75338ecb34d0e0c74020fce116e0cd496ba4c8b3
tukhoza <39373673+tukhoza@users.noreply.github.com>
2023-06-03 09:07:16 +0100

    Java specifiers

---
+++ Format specifiers/src/app/App.java
@@ -0,0 +1,22 @@
+package app;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Programm for format specifiers");
+		
+		int intNumber = 11;
+		float floatNumber = 12.0f;
+		long longNumber = 13;
+		double doubleNumber = 14.5;
+		String stringText = "The end";
+		
+		
+		System.out.printf("The Fromat for int %d", intNumber);
+		
+		
+
+	}
+
+}
